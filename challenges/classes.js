6f37e7261e21ee7d87d35aa73670fc2d19f9cee0
Kevin Smith <keveightysev@gmail.com>
2019-01-25 11:35:15 -0600

    Completed stetch goals.

--- challenges/classes.js
+++ challenges/classes.js
@@ -27,3 +27,22 @@ console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+
+class CubeMaker extends CuboidMaker {
+    constructor(props){
+        super(props);
+    }
+
+    surfaceArea() {
+        return 6 * this.length ** 2;
+    }
+}
+
+const cube = new CubeMaker({
+    length: 10,
+    width: 10,
+    height: 10,
+})
+
+console.log(cube.volume());
+console.log(cube.surfaceArea());
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -161,5 +161,7 @@ console.log(populationTotal);
 
 Stretch: If you haven't already, convert your array method callbacks into arrow functions.
 
+Already done!
+
 */
 
