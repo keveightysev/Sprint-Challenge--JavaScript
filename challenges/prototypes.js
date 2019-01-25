5cfe8bbf6c62ee5cb7900b314c1d004a07fb1723
Kevin Smith <keveightysev@gmail.com>
2019-01-25 12:25:23 -0600

    Refactored constructor in prototypes.js and classes in classes.js to simplify setting variable objects

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -6,10 +6,10 @@
   Create a constructor function named CuboidMaker that accepts properties for length, width, and height
 */
 
-function CuboidMaker(props) {
-  this.length = props.length;
-  this.width = props.width;
-  this.height = props.height;
+function CuboidMaker(length, width, height) {
+  this.length = length;
+  this.width = width;
+  this.height = height;
 }
 
 
@@ -39,11 +39,7 @@ CuboidMaker.prototype.surfaceArea = function() {
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
 
-const cuboid = new CuboidMaker({
-  length: 4,
-  width: 5,
-  height: 5,
-})
+const cuboid = new CuboidMaker(4,5,5)
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 console.log(cuboid.volume()); // 100
